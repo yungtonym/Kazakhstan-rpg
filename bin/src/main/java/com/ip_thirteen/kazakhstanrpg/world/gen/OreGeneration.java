5c7079b3228ca932350047d189e529b6380196ca
leonid_ivanov <lekhv.ivanov.2000@mail.ru>
2021-12-14 22:41:09 +0700

    Add ore generation

--- bin/src/main/java/com/ip_thirteen/kazakhstanrpg/world/gen/OreGeneration.java
+++ bin/src/main/java/com/ip_thirteen/kazakhstanrpg/world/gen/OreGeneration.java
@@ -15,19 +15,17 @@ import net.minecraft.world.gen.placement.Placement;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class OreGeneration {
-    // use this to generate ores in end stone since Vanilla doesnt have any that need it
+
     private static OreFeatureConfig.FillerBlockType ENDSTONE_FILLER = OreFeatureConfig.FillerBlockType
             .create("firstmod:ENDSTONE", "firstmod:endstone", new BlockMatcher(Blocks.END_STONE));
 
     public static void addOresToBiomes(){
         for (Biome biome : ForgeRegistries.BIOMES){
-            // make smile blocks generate in the nether from levels 20 to 40, as common as coal in veins of up to 25
             if (biome.getCategory() == Biome.Category.DESERT){
                 generate(biome, OreFeatureConfig.FillerBlockType.NATURAL_STONE, BlockInit.RUBY_ORE.get(),
                         20, 6, 40, 8);
             }
 
-            // make diamond blocks generate in the end
             if (biome.getCategory() == Biome.Category.FOREST){
                 generate(biome, ENDSTONE_FILLER, Blocks.DIAMOND_BLOCK, 10, 6, 40, 8);
             }
